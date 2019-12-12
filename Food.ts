0c35062e1d8d808c3a6923a8a2cd75dac531a9c1
TaylorRomanelli-Molloy <tromanelli@lions.molloy.edu>
2019-12-12 10:07:18 -0500

    1 file modified

--- Food.ts
+++ Food.ts
@@ -1,7 +1,29 @@
 import Actor from './Actor';
+import Point from './Point';
 
 class Food implements Actor
 {
+  private currentPosition: Point;
+  private isCurrentlyActive: boolean;
+
+  constructor(x:number,y:number)
+  {
+    this.currentPosition = new Point(x,y);
+    this.isCurrentlyActive = true;
+  } 
+  eat()
+  {
+    this.isCurrentlyActive = false;
+  }
+  get Position(): Point
+  {
+    return this.currentPosition;
+  }
+  get isActive(): boolean
+  {
+    return this.isCurrentlyActive;
+  }
+
   update()
   {
 
